09a200eca14825e5910161d646fe90343849371f
TashiNao <naoyatashima@icloud.com>
2022-07-10 11:47:43 +0900

    ユーザーテストコード

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,13 +5,10 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname, presence: true
-  validates :email, presence: true
-  validates :password, presence: true
-  validates :last_name, presence: true
-  validates :first_name, presence: true
-  validates :last_name_kana, presence: true
-  validates :first_name_kana, presence: true
+  validates :last_name, presence: true, format: {with: /\A[ぁ-んァ-ンー-龥]/}
+  validates :first_name, presence: true, format: {with: /\A[ぁ-んァ-ンー-龥]/}
+  validates :last_name_kana, presence: true, format: {with: /\A[ァ-ヶー－]+\z/}
+  validates :first_name_kana, presence: true, format: {with: /\A[ァ-ヶー－]+\z/ }
   validates :birth, presence: true
 
-  validates :email, uniqueness: true
 end
