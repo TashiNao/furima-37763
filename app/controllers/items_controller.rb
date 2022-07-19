7e897244e5d30e7fe7779ef7fa6f5d6463f2aa6c
TashiNao <naoyatashima@icloud.com>
2022-07-19 23:07:31 +0900

    JavaScript実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,6 @@
 class ItemsController < ApplicationController
+  before_action :move_to_signed_in, except: [:index]
+
   def index
     @item = Item.all
   end
@@ -18,7 +20,13 @@ class ItemsController < ApplicationController
 
   private
   def item_params
-    params.require(:item).permit(:product_name, :description, :image, :price).merge(user_id: current_user.id)
+    params.require(:item).permit(:product_name, :description, :image, :price, :category_id, :condition_id, :charge_id, :area_id, :delivery_day_id).merge(user_id: current_user.id)
+  end
+
+  def move_to_signed_in
+    unless user_signed_in?
+      redirect_to '/users/sign_in'
+    end
   end
 end
 
