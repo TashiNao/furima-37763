e4ca3dabc0a2a046b7512e5829ce2a44740acfe5
TashiNao <naoyatashima@icloud.com>
2022-07-30 12:49:13 +0900

    コードレビュ後の修正1

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -36,7 +36,10 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    if @item.destroy
+    if user_signed_in?
+      @item.destroy
+      redirect_to root_path
+    else
       redirect_to root_path
     end
   end
@@ -56,3 +59,4 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 end
+
