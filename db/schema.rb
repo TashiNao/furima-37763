7fff2c76556a7a1523ce80a623f29b4b7d593637
TashiNao <naoyatashima@icloud.com>
2022-07-30 21:56:53 +0900

    viewファイルの実装1

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_30_065026) do
+ActiveRecord::Schema.define(version: 2022_07_30_124940) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -34,7 +34,7 @@ ActiveRecord::Schema.define(version: 2022_07_30_065026) do
   end
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "psotal", null: false
+    t.string "postal", null: false
     t.integer "prefecture_id", null: false
     t.string "city", null: false
     t.string "street", null: false
