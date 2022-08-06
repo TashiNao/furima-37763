d12aad7a8983f99a83a0a91ad75ffd0de423118a
TashiNao <naoyatashima@icloud.com>
2022-08-06 21:16:25 +0900

    S3の修正

--- db/migrate/20220717141507_add_price_to_items.rb
+++
@@ -1,5 +0,0 @@
-class AddPriceToItems < ActiveRecord::Migration[6.0]
-  def change
-    add_column :items, :title, :string
-  end
-end
--- db/migrate/20220723065559_rename_product_name_column_to_title.rb
+++
@@ -1,5 +0,0 @@
-class RenameProductNameColumnToTitle < ActiveRecord::Migration[6.0]
-  def change
-    rename_column :items, :product_name, :title
-  end
-end
